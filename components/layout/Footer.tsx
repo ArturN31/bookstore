d6b43c97554c3facaebfa3333dc75e8b6cfefc74
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2025-01-03 14:52:23 +0000

    Footer: - code cleanup - style changes of social media btns

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,97 +1,72 @@
-import { Facebook, Instagram, Linkedin, Mail, MapPin, Phone, Twitter } from 'lucide-react';
+import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
 import Link from 'next/link';
 
 export const Footer = () => {
 	return (
 		<div className='flex bg-gunmetal text-white p-8'>
-			<div className='grid gap-5 m-auto'>
-				<p className='grid items-center text-center max-w-[500px]'>
-					We want to provide readers with a convenient and affordable way to access a vast library of digital books. We
-					strive to offer a diverse selection of genres, exclusive titles, and exceptional customer service, ensuring a
-					seamless and enjoyable reading experience for all.
-				</p>
+			<div className='grid gap-5 m-auto max-w-[850px]'>
+				<div className='grid items-center text-center'>
+					<p>
+						We want to provide readers with a convenient and affordable way to access a vast library of digital books.
+						We strive to offer a diverse selection of genres, exclusive titles, and exceptional customer service,
+						ensuring a seamless and enjoyable reading experience for all.
+					</p>
+				</div>
 
-				<div className='grid grid-flow-col auto-cols-auto gap-5'>
+				<div className='grid grid-flow-col auto-cols-fr gap-5'>
 					<div className='grid items-center'>
-						<ul className='grid gap-2'>
-							<li className='flex items-center gap-5'>
-								<MapPin />
+						<ul className='grid gap-2 text-center'>
+							{[
 								<div>
 									<p>West Highland Way, Milngavie</p>
 									<p>Glasgow, G62 6PB</p>
-								</div>
-							</li>
-							<li className='flex items-center gap-5'>
-								<Phone />
-								<p>+44 7911 123456</p>
-							</li>
-							<li className='flex items-center gap-5'>
-								<Mail />
+								</div>,
+								<p>+44 7911 123456</p>,
 								<a
 									href='mailto:books4you.contact@b4u.com'
 									className='text-sky-500 hover:text-sky-700'>
 									books4you.contact@example.com
-								</a>
-							</li>
+								</a>,
+							].map((content) => (
+								<li className='grid items-center gap-5'>{content}</li>
+							))}
 						</ul>
 					</div>
 
-					<div className='grid gap-3 text-center'>
+					<div className='grid gap-3 items-center text-center'>
 						<ul>
-							<li>
-								<a
-									href='/privacypolicy'
-									className='text-sky-500 hover:text-sky-700'>
-									Privacy Policy
-								</a>
-							</li>
-							<li>
-								<a
-									href='/tos'
-									className='text-sky-500 hover:text-sky-700'>
-									Terms of Service
-								</a>
-							</li>
-							<li>
-								<a
-									href='/shippinginfo'
-									className='text-sky-500 hover:text-sky-700'>
-									Shipping Information
-								</a>
-							</li>
-							<li>
-								<a
-									href='/returnpolicy'
-									className='text-sky-500 hover:text-sky-700'>
-									Return Policy
-								</a>
-							</li>
+							{[
+								{ text: 'Return Policy', url: '/returnpolicy' },
+								{ text: 'Privacy Policy', url: '/privacypolicy' },
+								{ text: 'Terms of Service', url: '/tos' },
+								{ text: 'Shipping Information', url: '/shippinginfo' },
+							].map((content) => (
+								<li>
+									<a
+										href={content.url}
+										className='text-sky-500 hover:text-sky-700'>
+										{content.text}
+									</a>
+								</li>
+							))}
 						</ul>
-
-						<div className='flex justify-center gap-2 items-center'>
-							<Link
-								href='https://www.linkedin.com/'
-								target='_blank'>
-								<Linkedin />
-							</Link>
-							<Link
-								href='https://www.facebook.com/'
-								target='_blank'>
-								<Facebook />
-							</Link>
-							<Link
-								href='https://www.instagram.com/'
-								target='_blank'>
-								<Instagram />
-							</Link>
-							<Link
-								className='px-1'
-								href='https://x.com/'
-								target='_blank'>
-								<Twitter />
-							</Link>
-						</div>
 					</div>
+				</div>
+
+				<div className='flex justify-center gap-8 items-center'>
+					{[<Linkedin />, <Facebook />, <Instagram />, <Twitter />].map((content) => (
+						<Link
+							className='border border-gunmetal rounded-full p-2 hover:border-white'
+							style={{ boxShadow: '0px 0px 6px black' }}
+							href='https://www.linkedin.com/'
+							target='_blank'>
+							{content}
+						</Link>
+					))}
+				</div>
+
+				<div className='grid justify-center font-thin text-center'>
+					<p>Books 4 You &copy; {new Date().getFullYear()}</p>
 				</div>
 			</div>
 		</div>
