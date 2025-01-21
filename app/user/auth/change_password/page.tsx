557122c70ed0a1b1b201f77fab453109e2d807b0
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2025-01-21 19:00:16 +0000

    Profile page: - added styling - change password btn that navigates to change password page

---
+++ app/user/auth/change_password/page.tsx
@@ -0,0 +1,11 @@
+import { RootLayout } from '@/components/layout/Layout';
+
+export default function ChangePasswordPage() {
+	return (
+		<RootLayout>
+			<div>
+				<p>Change Password Page</p>
+			</div>
+		</RootLayout>
+	);
+}
--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,6 +1,8 @@
 import { AddressForm } from '@/components/AddressForm';
 import { RootLayout } from '@/components/layout/Layout';
 import { createClient } from '@/utils/db/server';
+import { Pencil } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function UserProfile() {
 	const getUserProp = async (prop: keyof User) => {
@@ -47,27 +49,38 @@ export default async function UserProfile() {
 
 	return (
 		<RootLayout>
-			<div className='grid gap-5'>
-				<div>
-					<p>Name:</p>
-					<p>
-						{user.first_name} {user.last_name}
-					</p>
-				</div>
+			<div className='grid gap-5 justify-items-center'>
+				<Link
+					href={'/user/auth/change_password'}
+					className='flex w-fit rounded-md bg-slate-100 hover:bg-slate-200 shadow-[-0px_2px_4px_-2px_#000] hover:shadow-[0px_0px_4px_-2px_#000]'>
+					<div className='border border-black rounded-l-md px-2 py-1 text-lg'>Change Password</div>
+					<div className='border-y border-r border-black rounded-r-md  px-2 py-1'>
+						<Pencil />
+					</div>
+				</Link>
 
-				<div>
-					<p>Address:</p>
-					<p>{user.street_address}</p>
-					<p>{user.postcode}</p>
-					<p>
-						{user.city}, {user.country}
-					</p>
-				</div>
+				<div className='grid grid-cols-3 gap-5'>
+					<div className='border border-black text-center flex flex-col justify-center p-5 rounded-md shadow-[0px_2px_6px_-2px_#000]'>
+						<p className='font-semibold'>Name:</p>
+						<p>
+							{user.first_name} {user.last_name}
+						</p>
+					</div>
 
-				<div>
-					<p>Contact details:</p>
-					<p>{user.phone_number}</p>
-					<p>{userEmail}</p>
+					<div className='border border-black text-center flex flex-col justify-center p-5 rounded-md shadow-[0px_2px_6px_-2px_#000]'>
+						<p className='font-semibold'>Address:</p>
+						<p>{user.street_address}</p>
+						<p>{user.postcode}</p>
+						<p>
+							{user.city}, {user.country}
+						</p>
+					</div>
+
+					<div className='border border-black text-center flex flex-col justify-center p-5 rounded-md shadow-[0px_2px_6px_-2px_#000]'>
+						<p className='font-semibold'>Contact details:</p>
+						<p>{user.phone_number}</p>
+						<p>{userEmail}</p>
+					</div>
 				</div>
 			</div>
 		</RootLayout>
