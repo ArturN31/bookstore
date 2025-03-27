23571d9eeddfcb525284e828a56106963ca8d331
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2025-03-27 22:30:05 +0000

    Cart sidebar styling

--- components/layout/UserNavbar/CartBtn/CartBtn.tsx
+++ components/layout/UserNavbar/CartBtn/CartBtn.tsx
@@ -2,7 +2,7 @@
 
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { useRef, useState } from 'react';
-import { CartSidebar } from './CartSidebar';
+import { CartSidebar } from './CartSidebar/CartSidebar';
 
 export const CartBtn = ({
 	books,
@@ -16,28 +16,31 @@ export const CartBtn = ({
 		setOpenCart(!openCart);
 	};
 
-	const handleCloseCart = (e: React.FocusEvent<HTMLButtonElement>) => {
-		if (sidebarRef.current && !sidebarRef.current.contains(e.relatedTarget as Node)) {
-			setOpenCart(false);
-		}
-	};
+	const cartItemsAmount = books ? books.reduce((sum, book) => sum + book.quantity, 0) : 0;
 
 	return (
 		<>
 			<button
 				className='shadow-md rounded-full w-12 h-12 place-items-center grid bg-yellow hover:bg-yellow/[0.8] hover:border hover:border-black hover:cursor-pointer'
-				onClick={handleCartVisibility}
-				onBlur={handleCloseCart}>
+				onClick={handleCartVisibility}>
 				<ShoppingCartOutlinedIcon />
+				<div
+					className={`absolute translate-4 bg-red-500 rounded-full ${cartItemsAmount < 10 ? 'px-[6px]' : 'px-[3px]'}`}>
+					<p className='text-sm text-white'>{cartItemsAmount}</p>
+				</div>
 			</button>
 
-			{openCart && (
+			<div
+				className={`fixed top-0 right-0 z-50 h-full bg-white border-l border-black shadow-md overflow-y-auto transform transition-transform duration-700 ease-in-out ${
+					openCart ? 'translate-x-0' : 'translate-x-full'
+				}`}
+				style={{ width: 'auto', visibility: openCart ? 'visible' : 'hidden' }}
+				ref={sidebarRef}>
 				<CartSidebar
 					books={books}
 					setOpenCart={setOpenCart}
-					sidebarRef={sidebarRef}
 				/>
-			)}
+			</div>
 		</>
 	);
 };
---
+++ components/layout/UserNavbar/CartBtn/CartSidebar/CartSidebar.tsx
@@ -0,0 +1,53 @@
+import { Dispatch, SetStateAction } from 'react';
+import { CartSummary } from './CartSummary';
+import { CartHeader } from './CartHeader';
+import { CartItem } from './CartItem';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+export const CartSidebar = ({
+	books,
+	setOpenCart,
+}: {
+	books: { id: string; title: string; price: string; image_url: string; quantity: number }[];
+	setOpenCart: Dispatch<SetStateAction<boolean>>;
+}) => {
+	const handleCloseCart = () => {
+		setOpenCart(false);
+	};
+
+	return (
+		<div
+			tabIndex={-1}
+			aria-modal='true'
+			role='dialog'>
+			<CartHeader handleCloseCart={handleCloseCart} />
+			<div className='overflow-y-auto flex-grow'>
+				{books && books.length > 0 ? (
+					<ul className='space-y-4 p-4'>
+						{books.map((book) => (
+							<li key={book.id}>
+								<CartItem book={book} />
+							</li>
+						))}
+					</ul>
+				) : (
+					<div className='text-center py-10 grid gap-3'>
+						<ShoppingCartIcon sx={{ color: '#6a7282', margin: 'auto' }} />
+						<p className='text-gray-500'>Your cart is currently empty.</p>
+					</div>
+				)}
+			</div>
+
+			{books && books.length > 0 && (
+				<div className='bg-gray-100'>
+					<CartSummary books={books} />
+					<div className='border-y p-4'>
+						<button className='w-full bg-gunmetal/[0.9] hover:bg-gunmetal text-white font-semibold py-3 rounded-md transition-colors duration-200'>
+							Proceed to Checkout
+						</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
